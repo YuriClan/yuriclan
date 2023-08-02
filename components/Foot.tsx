df165192f8fa07a683f06073b1dcefbb30bf956d
Mihir <aminvasudev6@gmail.com>
2023-08-03 00:18:10 +0530

    Foot Links

--- components/Foot.tsx
+++ components/Foot.tsx
@@ -8,6 +8,7 @@ FooterLink,
 Heading,
 } from "components/FooterStyles";
 import style from '../styles/style.module.css'
+import Image from "next/image";
 
 const Footer = () => {
 return (
@@ -17,28 +18,28 @@ return (
 	</h1>
 	<Container>
 		<Row>
+		<Column className={style.columns}>
+			<Heading className={style.footerHeading}>Product</Heading>
+			<FooterLink href="#">Features</FooterLink>
+			<FooterLink href="#">Updates</FooterLink>
+			<FooterLink href="#">Integration</FooterLink>
+			<FooterLink href="#">Support</FooterLink>
+		</Column>
 		<Column>
-			<Heading>About Us</Heading>
+			<Heading className={style.footerHeading}>About Us</Heading>
 			<FooterLink href="#">Aim</FooterLink>
 			<FooterLink href="#">Vision</FooterLink>
 			<FooterLink href="#">Testimonials</FooterLink>
 		</Column>
 		<Column>
-			<Heading>Services</Heading>
-			<FooterLink href="#">Writing</FooterLink>
-			<FooterLink href="#">Internships</FooterLink>
-			<FooterLink href="#">Coding</FooterLink>
-			<FooterLink href="#">Teaching</FooterLink>
+			<Heading className={style.footerHeading}>Developers</Heading>
+			<FooterLink href="#">Documentation</FooterLink>
+			<FooterLink href="#">API reference</FooterLink>
+			<FooterLink href="#">API status</FooterLink>
+			<FooterLink href="#">Open Source</FooterLink>
 		</Column>
-		<Column>
-			<Heading>Contact Us</Heading>
-			<FooterLink href="#">Uttar Pradesh</FooterLink>
-			<FooterLink href="#">Ahemdabad</FooterLink>
-			<FooterLink href="#">Indore</FooterLink>
-			<FooterLink href="#">Mumbai</FooterLink>
-		</Column>
-		<Column>
-			<Heading>Social Media</Heading>
+		{/* <Column>
+			<Heading className={style.footerHeading}>Social Media</Heading>
 			<FooterLink href="#">
 			<i className="fab fa-facebook-f">
 				<span style={{ marginLeft: "10px" }}>
@@ -67,8 +68,14 @@ return (
 				</span>
 			</i>
 			</FooterLink>
-		</Column>
+		</Column> */}
+        <div className={style.footLogo}>
+            <Image src="/pyro_red.png" alt="YuriClan" width={200} height={650}/>
+        </div>
 		</Row>
+        <div className="flex flex-col border-t border-gray-300 py-2 lg:py-3 mt-8 lg:mt-12 lg:flex-row lg:justify-between lg:items-center">
+            hey
+        </div>
 	</Container>
 	</Box>
 );
